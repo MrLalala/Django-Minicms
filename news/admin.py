95751309411cb957b5d5bc800bf2d9544d702884
Gan <1354410847@qq.com>
2017-12-03 16:54:10 +0800

    a failure

--- news/admin.py
+++ news/admin.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Article, Column
 
+
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'intro')
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'pub_date', 'update_date')
+
+
+admin.site.register(Column, ColumnAdmin)
+admin.site.register(Article, ArticleAdmin)
 # Register your models here.
